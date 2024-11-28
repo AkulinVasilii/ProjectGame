429a9a0ddee697778815eba4a09d9ea7095eb063
AkulinVasilii <akulin.ve@phystech.edu>
2024-11-28 07:33:46 +0300

    commit

--- Game/main.py
+++ Game/main.py
@@ -14,9 +14,6 @@ FPS = 90
 # Imagine the colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
 # Start the game and creation of a screen
